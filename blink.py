b583fbbcc21afbef0de519ea0c110336d4ea17c0
Antônio José <137100266+AntonioStorm@users.noreply.github.com>
2024-11-28 16:06:31 -0300

    Update blink.py

--- blink.py
+++ blink.py
@@ -1,9 +1,10 @@
-import time
+#Importações das bibliotecas
+import time  
 import RPi.GPIO as gpio
-
+#Seta os pinos como numeração na placa e utiliza o pino 10
 gpio.setmode(gpio.BOARD)
 gpio.setup(10, gpio.OUT)
-
+#Código de piscar o LED que estará ligado no pino 10
 while True:
 	gpio.output(10, True)
 	time.sleep(1)
